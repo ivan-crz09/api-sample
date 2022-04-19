f8906ff1989f48348ee241f5fdf15b2015c2069c
Ivan Cruz <ivan.crz09@gmail.com>
2022-04-19 02:12:59 -0500

    Added more test and endpoints

--- README.md
+++ README.md
@@ -14,7 +14,23 @@ It's an small api built with NodeJS and Express
 
 3.- Run `npm start`
 
+
 **Note** it will run for defect in port `3010`. You can change the default port inside start.js
+
+#### Current endpoints
+
+Get Hello Request:
+```
+curl --location --request GET 'http://localhost:3010/hello'
+```
+
+Get Pokemon Request
+```
+curl --location --request GET 'http://localhost:3010/pokemon'
+```
+
+
+
 
 ### Running Test
 
--- controllers/pokemon.test.js
+++ controllers/pokemon.test.js
@@ -7,8 +7,14 @@ test('gets hello message', async () => {
 })
 
 
-test('get 20 results when not send any param', async () => {
+test('get default results when not send any limit', async () => {
     const response = await request.get('/pokemon')
     expect(response.status).toEqual(200)
     expect(response.body.results.length).toEqual(20)
+})
+
+test('get 30 results when send a limit', async () => {
+    const response = await request.get('/pokemon?limit=30')
+    expect(response.status).toEqual(200)
+    expect(response.body.results.length).toEqual(30)
 })
